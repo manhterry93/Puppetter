85ad1d4ab2ff8d7aa989c95babcec9f0cb9ff483
manhpv <manhterry93@gmail.com>
2021-02-09 02:27:05 +0700

    crawl product info from tiki hot deal

--- index.js
+++ index.js
@@ -1,15 +1,79 @@
 const puppeteer = require('puppeteer');
 
+
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });;
+    const browser = await puppeteer.launch({
+        headless: true, defaultViewport: null, args: [
+            '--window-size=1920,1080',
+        ]
+    });;
     const page = await browser.newPage();
-    await page.goto('https://zshop.vn/lenovo-thinkpad-t14-2020-14-inch-amd-ryzen-7-pro-4750u-16gb-512gb-fhd.html', { waitUntil: 'networkidle2' });
-    //   await waitTillHTMLRendered(page)
-    await waitTillHTMLRendered(page);
-    //   await page.pdf({path: 'hn.pdf', format: 'A4'});
-    await page.screenshot({path: 'full.png', fullPage: true});
+    await page.setViewport({
+        width: 1920,
+        height: 1080
+    });
+    await page.goto('https://tiki.vn/deal-hot/', { waitUntil: 'networkidle2' });
+
+    // await page.screenshot({ path: 'full.png', fullPage: true });
+
+    let data = await page.evaluate(() => {
+
+        let products = [];
+        let product_wrapper = document.querySelectorAll('.Item__Wrapper-m1oy8w-0');
+        product_wrapper.forEach((product) => {
+            let dataJson = {};
+            try {
+                dataJson.img = product.querySelector('.PictureV2__StyledWrapImage-tfuu67-0 > img').src;
+                dataJson.title = product.querySelector('.title').innerText;
+                price = product.querySelector('.price').innerText;
+                // Remove redurant part
+                price = price.replace(/₫/g, '').replace(/\%/g, ' ').replace(/\s+/g, ' ').trim();
+                priceArr = price.split(' ');
+                dataJson.price = priceArr[0];
+                dataJson.originPrice = priceArr[1];
+                // dataJson.originPrice = product.querySelector('.original').innerText.replace(/₫/g,'').trim();
+            } catch (err) {
+                console.log('error: ', err);
+            }
+            products.push(dataJson)
+        });
+        return products;
+    });
+
+    console.log(data);
     await browser.close();
 })();
+
+
+async function scrapeInfiniteScrollItems(
+    page,
+    extractItems,
+    itemTargetCount,
+    scrollDelay = 1000,
+) {
+    let items = [];
+    try {
+        let previousHeight;
+        while (items.length < itemTargetCount) {
+            items = await page.evaluate(extractItems);
+            previousHeight = await page.evaluate('document.body.scrollHeight');
+            await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+            await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+            await page.waitFor(scrollDelay);
+        }
+    } catch (e) { }
+    return items;
+}
+
+
+function extractItems() {
+    const extractedElements = document.querySelectorAll('#boxes > div.box');
+    const items = [];
+    for (let element of extractedElements) {
+        items.push(element.innerText);
+    }
+    return items;
+}
 
 
 const waitTillHTMLRendered = async (page, timeout = 30000) => {
